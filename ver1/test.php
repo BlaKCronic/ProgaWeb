bb7da3213f7668f38f10f168478e735471c0d099
BlaKCronic <poncegonzalez849@gmail.com>
2025-09-22 10:52:32 -0600

    insert-test

--- ver1/test.php
+++ ver1/test.php
@@ -1,6 +1,9 @@
 <?php
 require_once "models/institucion.php";
 $app = new Institucion;
-$filas_afectadas = $app -> delete(1);
-print_r($filas_afectadas);
+//$filas_afectadas = $app -> delete(1);
+$data ['institucion'] = "Instituto Tecnologico de Tijuana";
+$data ['logotipo'] = "logotipo_prueba.png";
+$filas_afectadas = $app -> create($data);
+//print_r($filas_afectadas);
 ?>
