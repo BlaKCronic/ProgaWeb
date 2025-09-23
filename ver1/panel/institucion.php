d2d4e2df85a75451f1f8a26438dadf2d2116d617
BlaKCronic <poncegonzalez849@gmail.com>
2025-09-23 09:53:35 -0600

    paneles

---
+++ ver1/panel/institucion.php
@@ -0,0 +1,34 @@
+<?php
+require_once "../models/institucion.php";
+$app = new Institucion();
+$action = isset($_GET['action']) ? $_GET['action'] : 'read';
+$data = array();
+switch ($action){
+    case 'create':
+        $data['institucion'] = $_POST['institucion'];
+        $data['logotipo'] = $_POST['logotipo'];
+        $filas = $app -> create($data);
+        echo $filas;
+        break;
+    
+    case 'update':
+        $data['institucion'] = $_POST['institucion'];
+        $data['logotipo'] = $_POST['logotipo'];
+        $id = $_POST['id_institucion'];
+        $filas = $app -> update($data, $id);
+        echo $filas;
+        break;
+
+    case 'delete':
+        $id = $_GET['id_institucion'];
+        $filas = $app -> delete($id);
+        echo $filas;
+        break;
+    
+    case 'read':
+    default:
+        $data = $app -> read();
+        print_r($data);
+        break;
+}
+?>
